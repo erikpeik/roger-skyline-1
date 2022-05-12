5adb1556576441d513c040bf3e2032209d969f81
Erik Mende <52178013+erikpeik@users.noreply.github.com>
2022-05-12 19:19:44 +0300

    Added sleep to script
    I had to add sleep to this script because sometimes when you restart your computer it cannot connect to Debian servers. 5-second sleep seems to fix things.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo -n 'Updating Packages ' >> /var/log/update_script.log
+sleep 5
 echo `date` >> /var/log/update_script.log
 echo `sudo apt-get update --yes` >> /var/log/update_script.log
 echo `sudo apt-get upgrade --yes` >> /var/log/update_script.log
