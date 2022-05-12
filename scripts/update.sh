f296c312a458f59d6dd013c6274894acadfe93a8
Erik Mende <52178013+erikpeik@users.noreply.github.com>
2022-05-12 19:32:09 +0300

    Changed sleep to right spot, printing datetime before the sleep

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo -n 'Updating Packages ' >> /var/log/update_script.log
-sleep 5
 echo `date` >> /var/log/update_script.log
+sleep 5
 echo `sudo apt-get update --yes` >> /var/log/update_script.log
 echo `sudo apt-get upgrade --yes` >> /var/log/update_script.log
 echo '' >> /var/log/update_script.log
